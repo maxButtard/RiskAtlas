5c0de12e6fb50f14b8524327088173f1815490fe
Maxence Buttard <buttardmaxence2@icloud.com>
2026-04-21 15:32:07 +0200

    remove backend from repo

--- src/backend/config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  5 23:09:53 2026
-
-@author: buttard
-"""
-
-FILE_PATH = "data/raw/Matrice Ope.xlsx"
-OUTPUT_PATH = "data/processed/final_dataset_github.xlsx"
